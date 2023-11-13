62583a14c1b7b828d784a41f113d3c29b7d51a2c
ALUNO <ALUNO@ETEJK-LAB04-M07>
2023-11-13 19:41:45 -0300

    Inicio da criação dos produtos

---
+++ pdv/src/main/java/br/com/pdv/controller/ProdutoController.java
@@ -0,0 +1,20 @@
+package br.com.pdv.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import br.com.pdv.repository.ProdutosRepository;
+
+@Controller
+
+public class ProdutoController {
+	private ProdutosRepository produtoRepository;
+	
+	@GetMapping({"/produtos"})
+	public String home(ModelMap model) {
+		model.addAttribute("lojas", produtoRepository.findAll());
+		return "produtos";
+	}
+
+}
---
+++ pdv/src/main/java/br/com/pdv/entidades/ProdutoEntidade.java
@@ -0,0 +1,61 @@
+package br.com.pdv.entidades;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name ="loja", schema = "pdv")
+public class ProdutoEntidade implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@Column(name = "id")
+	private Long id;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	@Column(name = "nome")
+	private String nome;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	@Column(name = "quantidade")
+	private Long quantidade;
+
+	public Long getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(Long quantidade) {
+		this.quantidade = quantidade;
+	}
+	@Column(name = "preco")
+	private Double preco;
+
+	public Double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+
+
+
+}
