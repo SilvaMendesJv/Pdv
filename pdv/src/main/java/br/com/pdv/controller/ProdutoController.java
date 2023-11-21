8139310e4b3ab9d7897f17ef73fe3e3ac83e0a88
ALUNO <ALUNO@ETEJK-LAB04-M07>
2023-11-21 20:03:26 -0300

    termino do html de produtos e resolução de erros.

--- pdv/src/main/java/br/com/pdv/controller/ProdutoController.java
+++ pdv/src/main/java/br/com/pdv/controller/ProdutoController.java
@@ -1,5 +1,6 @@
 package br.com.pdv.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +10,7 @@ import br.com.pdv.repository.ProdutosRepository;
 @Controller
 
 public class ProdutoController {
+	@Autowired
 	private ProdutosRepository produtoRepository;
 	
 	@GetMapping({"/produtos"})
--- pdv/src/main/java/br/com/pdv/entidades/ProdutoEntidade.java
+++ pdv/src/main/java/br/com/pdv/entidades/ProdutoEntidade.java
@@ -8,7 +8,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name ="loja", schema = "pdv")
+@Table(name ="produto", schema = "pdv")
 public class ProdutoEntidade implements Serializable{
 	
 	private static final long serialVersionUID = 1L;
